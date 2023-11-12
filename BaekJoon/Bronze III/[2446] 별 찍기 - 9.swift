f1b48ade131fcd17cf043b37f5bfce7ffad5e07e
Oliver03710 <jhee2619@gmail.com>
2023-11-12 19:47:32 +0900

    [BOJ][Bronze III][2446] 별 찍기 - 9

---
+++ BaekJoon/Bronze III/[2446] 별 찍기 - 9.swift
@@ -0,0 +1,17 @@
+let n = Int(readLine()!)!
+
+var answer = Array<String>()
+
+(1...n).forEach { i in
+    let spaces = Array(repeating: " ", count: n - i).joined()
+    let stars = Array(repeating: "*", count: 2 * i - 1).joined()
+    answer.append(spaces + stars)
+}
+
+answer.reverse()
+let last = answer.popLast()!
+let reversed = answer.reversed()
+
+answer.append(last)
+answer.append(contentsOf: reversed)
+answer.forEach { print($0) }
