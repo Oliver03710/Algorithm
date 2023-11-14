4affbc795ea8f5732e56dd878a1c9acb773d1397
Oliver03710 <jhee2619@gmail.com>
2023-11-14 20:58:39 +0900

    [BOJ][Bronze III][2460] 지능형 기차 2

---
+++ BaekJoon/Bronze III/[2460] 지능형 기차 2.swift
@@ -0,0 +1,15 @@
+var sum = 0
+var ans = Array<Int>()
+
+for _ in 0..<10 {
+    if let input = readLine() {
+        var people = input
+            .split(separator: " ")
+            .map { Int(String($0))! }
+        
+        sum += people[1] - people[0]
+        ans.append(sum)
+    }
+}
+
+print(ans.max()!)
